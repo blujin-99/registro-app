400ed7a4d0f32610f4299bdd2f0970da5bdcf80c
Matias Lenci <malenci@santafe.gov.ar>
2023-07-27 12:04:37 -0300

    add maquetacion tabla pendientes

--- src/app/modules/inicio/components/tabla-pendientes/tabla-pendientes.component.ts
+++ src/app/modules/inicio/components/tabla-pendientes/tabla-pendientes.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TablaTramiteService } from '../../services/tabla-tramite.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tabla-pendientes',
   templateUrl: './tabla-pendientes.component.html',
-  styleUrls: ['./tabla-pendientes.component.scss']
+  styleUrls: ['./tabla-pendientes.component.scss'],
 })
-export class TablaPendientesComponent {
+export class TablaPendientesComponent implements OnInit {
+  displayedColumns: string[] = [
+    'TRAMITE',
+    'NÚMERO DE FORMULARIO',
+    'JURISDICCIÓN',
+    'ESTADO TASAS',
+    'ESTADO EXCEDENTES',
+  ];
+  dataSource = new MatTableDataSource();
+  pendientes!: any;
 
+  constructor(private tablaSrv: TablaTramiteService) {}
+  ngOnInit(): void {
+    this.tablaSrv.getTablaPendientes().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatTabsModule],
-  exports: [MatTabsModule],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  exports: [MatTabsModule, MatTableModule, MatInputModule, MatFormFieldModule],
 })
 export class MaterialModule {}
