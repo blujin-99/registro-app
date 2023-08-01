740865a8bc06d4bd71c2c64939c90c28c08a8f72
Matias Lenci <malenci@santafe.gov.ar>
2023-08-01 09:00:03 -0300

    add maquetacion nuevo tramite

--- src/app/modules/nuevo-tramite/nuevo-tramite.module.ts
+++ src/app/modules/nuevo-tramite/nuevo-tramite.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NuevoTramitePageComponent } from './pages/nuevo-tramite-page/nuevo-tramite-page.component';
 import { NuevoTramiteRoutingModule } from './nuevo-tramite-routing.module';
+import { SideBarComponent } from 'src/app/core/layout/side-bar/side-bar.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [NuevoTramitePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SideBarComponent, MaterialModule],
   exports: [NuevoTramiteRoutingModule, NuevoTramitePageComponent],
 })
 export class NuevoTramiteModule {}
