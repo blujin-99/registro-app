8536a0172f711dc1f46ebf81110414d4bbc7d87d
Matias Lenci <malenci@santafe.gov.ar>
2023-08-01 09:34:16 -0300

    add env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,18 @@
+const baseURL: string =
+  'http://10.1.46.32:5656/formelectronico/web/app_dev.php/API/v2/';
+
+export const environment = {
+  production: false,
+  baseURL: baseURL,
+  tramites: baseURL + 'usuario/tramite', //Listado de tramite del Usuario - Listado de trámites del Usuario y la búsqueda va por get  sobre esta url
+  estadoTasas: baseURL + 'EstadoTasas',
+  estadoTramite: baseURL + 'EstadoTramite',
+  estadoExcedentes: baseURL + 'EstadoExcedentes',
+  categoria: baseURL + 'usuario/TipoCategoriaTramite',
+  tramiteServicio: baseURL + 'usuario/TipoTramiteService',
+  jurisdiccion:
+    'https://twww.santafe.gov.ar/tramites/formelectronico/API/getStatic/Jurisdiccion?orderby=nombre',
+  app: {
+    nombre: 'https://twww.santafe.gov.ar/mjydh-web/api',
+  },
+};
