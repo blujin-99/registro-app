7d560e87d8189319519d5de8269463412396dda9
Matias Lenci <malenci@santafe.gov.ar>
2023-08-02 12:00:31 -0300

    add maquetacion pagina consulta de pagos

--- src/app/modules/consulta-pagos/consulta-pagos.module.ts
+++ src/app/modules/consulta-pagos/consulta-pagos.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConsultaPagosPageComponent } from './pages/consulta-pagos-page/consulta-pagos-page.component';
 import { ConsultaPagosRoutingModule } from './consulta-pagos-routing.module';
+import { SideBarComponent } from 'src/app/core/layout/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [ConsultaPagosPageComponent],
-  imports: [CommonModule, ConsultaPagosRoutingModule],
+  imports: [CommonModule, ConsultaPagosRoutingModule, SideBarComponent],
   exports: [ConsultaPagosRoutingModule, ConsultaPagosPageComponent],
 })
 export class ConsultaPagosModule {}
