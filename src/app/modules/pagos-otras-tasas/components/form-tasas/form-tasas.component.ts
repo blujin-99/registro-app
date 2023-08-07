ab95583b55b29b838c3f1c64d88d93e8ed9a3f19
Matias Lenci <malenci@santafe.gov.ar>
2023-08-07 11:39:55 -0300

    add form de pago de otras tasas

---
+++ src/app/modules/pagos-otras-tasas/components/form-tasas/form-tasas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-tasas',
+  templateUrl: './form-tasas.component.html',
+  styleUrls: ['./form-tasas.component.scss']
+})
+export class FormTasasComponent {
+
+}
--- src/app/modules/pagos-otras-tasas/pages/pagos-otras-tasas-page/pagos-otras-tasas-page.component.ts
+++ src/app/modules/pagos-otras-tasas/pages/pagos-otras-tasas-page/pagos-otras-tasas-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { PagosTasasService } from '../../services/pagos-tasas.service';
 
 @Component({
   templateUrl: './pagos-otras-tasas-page.component.html',
-  styleUrls: ['./pagos-otras-tasas-page.component.scss']
+  styleUrls: ['./pagos-otras-tasas-page.component.scss'],
+  providers: [PagosTasasService],
 })
-export class PagosOtrasTasasPageComponent {
-
-}
+export class PagosOtrasTasasPageComponent {}
