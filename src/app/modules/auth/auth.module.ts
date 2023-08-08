9d4b7f03f6249a064c21eaf34f796e0b6e63dfbc
Matias Lenci <malenci@santafe.gov.ar>
2023-08-08 12:53:12 -0300

    add modulo auth y update nombre proyecto

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+import { AuthRoutingModule } from './auth-routing.module';
+
+@NgModule({
+  declarations: [AuthPageComponent],
+  imports: [CommonModule],
+  exports: [AuthPageComponent, AuthRoutingModule],
+})
+export class AuthModule {}
