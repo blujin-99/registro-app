1554d48cf4564846ab08c7fc699c9d06676fdb40
dmosqueda <dmosqueda@santafe.gob.ar>
2023-08-09 13:19:54 -0300

    interceptor httpErrorResponse

--- src/app/core/interceptor/not-found.interceptor.ts
+++ src/app/core/interceptor/not-found.interceptor.ts
@@ -4,24 +4,32 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
+  HttpErrorResponse,
+  HttpHeaders
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs';
-
+import { ConsultaMesaEntradaService } from 'src/app/shared/services/consulta-mesa-entrada.service';
 @Injectable()
-
 export class NotFoundError implements HttpInterceptor {
 
+  constructor(private mesaEntradaService: ConsultaMesaEntradaService) {}
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      catchError((error: HttpErrorResponse) => {
-        if(error.status === 404){
-          return throwError(() =>console.log( 'not fuond'))
-        }
-        console.error('Error Global', error)
-        return throwError(() => error)
-      })
-    )
+
+    const headers = new HttpHeaders({
+      'token-usuario': 'AB5646dfftgRE333'
+    });
+  
+    const requestClone = request.clone({
+      headers
+    });
+    
+    return next.handle(requestClone).pipe(
+       catchError((error: HttpErrorResponse) => {
+         this.mesaEntradaService.showError(error.status);
+         return throwError(() => 'Ha ocurrido un error' + ' tipo: ' + error.status);
+       })
+    );
   }
 }
--- src/app/core/layout/catching-error/catching-error.component.ts
+++ src/app/core/layout/catching-error/catching-error.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ConsultaMesaEntradaService } from 'src/app/shared/services/consulta-mesa-entrada.service';
 
 @Component({
   selector: 'app-catching-error',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./catching-error.component.scss']
 })
 export class CatchingErrorComponent {
+
+  error$ = this.mesaEntradaService.error$;
+  private errorSubscription: Subscription | undefined;
+
+  mensaje: string | null = null; 
+
+  constructor(private mesaEntradaService: ConsultaMesaEntradaService) {
+
+  }
+
+  closeAlert() {
+    this.mesaEntradaService.clearErro()
+  }
+
+  ngOnInit(): void {
+    this.errorSubscription = this.mesaEntradaService.error$.subscribe(error => {
+      if (error =='404') {
+        this.mensaje = "No se ha encontrado el Tramite "
+      }
+      if (error == '500') {
+        this.mensaje = "Error de conexión, intente nuevamente más tarde"
+      }
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    if(this.errorSubscription){
+       this.errorSubscription.unsubscribe()
+    }
+    this.closeAlert()
+  }
 
 }
