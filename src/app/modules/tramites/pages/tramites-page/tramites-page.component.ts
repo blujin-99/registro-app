95acb8eb77c517fc857f5789d732606148f71b8c
Matias Lenci <malenci@santafe.gov.ar>
2023-08-09 08:31:15 -0300

    update pagin de inicio y nueva pagina de mis tramites

--- src/app/modules/tramites/pages/tramites-page/tramites-page.component.ts
+++ src/app/modules/tramites/pages/tramites-page/tramites-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TramitesService } from '../../services/tramites.service';
 
 @Component({
   templateUrl: './tramites-page.component.html',
-  styleUrls: ['./tramites-page.component.scss']
+  styleUrls: ['./tramites-page.component.scss'],
+  providers: [TramitesService],
 })
-export class TramitesPageComponent {
+export class TramitesPageComponent implements OnInit {
+  cargando: boolean = true;
+  constructor(private tramiteSrv: TramitesService) {}
+  ngOnInit(): void {
+    this.tramiteSrv.getCategorias();
+    this.tramiteSrv.getEstadoTramite();
+    this.tramiteSrv.getEstadoTasas();
+    this.tramiteSrv.getJurisdiccion();
+    this.tramiteSrv.getEstadoExcedentes();
+    //TODO: Mover getTramiteServicio a filtro component
 
+    this.cargando = false;
+  }
 }
--- src/app/modules/tramites/tramites.module.ts
+++ src/app/modules/tramites/tramites.module.ts
@@ -2,10 +2,39 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TramitesPageComponent } from './pages/tramites-page/tramites-page.component';
 import { TramiteRoutingModule } from './tramites-routing.module';
+import { ChipFiltroComponent } from './components/chip-filtro/chip-filtro.component';
+import { FiltrosBusquedaComponent } from './components/filtros-busqueda/filtros-busqueda.component';
+import { BotonesTramiteComponent } from './components/botones-tramite/botones-tramite.component';
+import { TablaPendientesComponent } from './components/tabla-pendientes/tabla-pendientes.component';
+import { TablaEntregadoComponent } from './components/tabla-entregado/tabla-entregado.component';
+import { TablaFinalizadoComponent } from './components/tabla-finalizado/tabla-finalizado.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SideBarComponent } from 'src/app/core/layout/side-bar/side-bar.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AlertCategoriaComponent } from 'src/app/shared/components/alert-categoria/alert-categoria.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [TramitesPageComponent],
-  imports: [CommonModule, TramiteRoutingModule],
+  declarations: [
+    TramitesPageComponent,
+    ChipFiltroComponent,
+    FiltrosBusquedaComponent,
+    BotonesTramiteComponent,
+    TablaPendientesComponent,
+    TablaEntregadoComponent,
+    TablaFinalizadoComponent,
+  ],
+  imports: [
+    CommonModule,
+    TramiteRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SideBarComponent,
+    MaterialModule,
+    AlertCategoriaComponent,
+    MatFormFieldModule,
+  ],
   exports: [TramiteRoutingModule, TramitesPageComponent],
 })
 export class TramitesModule {}
