8cb7ccda8407c410509d245d7f3c6f5e328c24e9
Matias Lenci <malenci@santafe.gov.ar>
2023-08-09 12:37:13 -0300

    add auth2

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authConfig } from '../config/auth.config';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authConfig); // Cargo parámetros de configuración
+    this.oauthService.setStorage(localStorage); // Almacena los tokens en el almacenamiento local
+    //this.oauthService.tryLogin(); // Intenta realizar un inicio de sesión automático al cargar la aplicación
+  }
+
+  login() {
+    // Redirige al servidor CAS para iniciar el flujo de autorización
+    this.oauthService.initCodeFlow();
+    this.oauthService.initImplicitFlow();
+  }
+
+  logout() {
+    // Cierra la sesión actual y redirige al servidor CAS para cerrar la sesión allí también
+    this.oauthService.logOut();
+    localStorage.clear();
+  }
+
+  isLoggedIn(): boolean {
+    // Verifica si el usuario está autenticado
+    return this.oauthService.hasValidAccessToken();
+  }
+
+  getAccessToken(): string {
+    // Obtiene el token de acceso actual
+    return this.oauthService.getAccessToken();
+  }
+}
