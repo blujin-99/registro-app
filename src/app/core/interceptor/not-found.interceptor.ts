780497aa6d73bbdbd516ac2fe607e33eb3aa1912
Matias Lenci <malenci@santafe.gov.ar>
2023-08-10 12:01:09 -0300

    fix error interceptor

--- src/app/core/interceptor/not-found.interceptor.ts
+++ src/app/core/interceptor/not-found.interceptor.ts
@@ -5,31 +5,34 @@ import {
   HttpEvent,
   HttpInterceptor,
   HttpErrorResponse,
-  HttpHeaders
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs';
 import { ConsultaMesaEntradaService } from 'src/app/shared/services/consulta-mesa-entrada.service';
 @Injectable()
 export class NotFoundError implements HttpInterceptor {
-
   constructor(private mesaEntradaService: ConsultaMesaEntradaService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    // const headers = new HttpHeaders({
+    //   'token-usuario': 'AB5646dfftgRE333'
+    // });
 
-    const headers = new HttpHeaders({
-      'token-usuario': 'AB5646dfftgRE333'
-    });
-  
-    const requestClone = request.clone({
-      headers
-    });
-    
-    return next.handle(requestClone).pipe(
-       catchError((error: HttpErrorResponse) => {
-         this.mesaEntradaService.showError(error.status);
-         return throwError(() => 'Ha ocurrido un error' + ' tipo: ' + error.status);
-       })
+    // const requestClone = request.clone({
+    //   headers
+    // });
+
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.mesaEntradaService.showError(error.status);
+        return throwError(
+          () => 'Ha ocurrido un error' + ' tipo: ' + error.status
+        );
+      })
     );
   }
 }
