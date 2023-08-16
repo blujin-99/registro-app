658a143d2333838cfec22cf382e637803cdb9abe
Matias Lenci <malenci@santafe.gov.ar>
2023-08-16 09:50:18 -0300

    add mock de tramites

--- src/app/modules/tramites/services/tabla-tramite.service.ts
+++ src/app/modules/tramites/services/tabla-tramite.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Tramites } from 'src/app/core/mocks/tramites.mock';
 
 @Injectable({
   providedIn: 'root',
@@ -8,71 +9,6 @@ export class TablaTramiteService {
   constructor() {}
 
   getTablaPendientes(): Observable<any> {
-    let pendientes = [
-      {
-        id: 0,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000902,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 1,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000903,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 2,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000904,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 3,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000905,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 4,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000906,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 5,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000907,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-      {
-        id: 6,
-        pagar: false,
-        tramite: 'CERT',
-        numeroFormulario: 2023000908,
-        jurisdiccion: 'ROSARIO',
-        tasas: 'NO PAGADO',
-        excedentes: 'NO CORRESPONDE',
-      },
-    ];
-    return of(pendientes);
+    return of(Tramites.rows);
   }
 }
