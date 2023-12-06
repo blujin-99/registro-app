290c1492be4c4d8f99d6f115a534a40f5a08650b
dmosqueda <dmosqueda@santafe.gov.ar>
2023-12-06 07:41:04 -0300

    traer los datos una sola vez de los tramites

--- src/app/modules/tramites/services/filtros.service.ts
+++ src/app/modules/tramites/services/filtros.service.ts
@@ -50,81 +50,76 @@ export class FiltrosService {
    * @function getTablaFiltrada() devuleve un array de los datos de la tabla
    * con la información filtrada
    */
-  getTablaFiltrada(): any[] {
+  getTablaFiltrada(): Observable<any[]> {
     const filtrosRow = this.filtrosSubject.value;
-    //this.datosTabla se convirtió en observable la cual va a ser escuchada por los otros métodos
-    return this.datosTabla.value.filter((fila: any) => {
-      return filtrosRow.every((filtro) => {
-        if (filtro.length != 0) {
-          if (filtro.busqueda && filtro.busqueda.descripcion) {
-            // Si hay un valor de búsqueda por número de tramite en los filtros, usarlo para buscar por los últimos dígitos
-            return (
-              fila.codigo_tramite.endsWith(filtro.busqueda.descripcion) ||
-              fila.TipoTramiteServicio.nombre === filtro.nombre ||
-              fila.EstadoTasas.descripcion === filtro.descripcion ||
-              fila.EstadoExcedentes.descripcion === filtro.descripcion ||
-              fila.Jurisdiccion.nombre === filtro.nombre ||
-              fila.EstadoTramite.descripcion === filtro.descripcion
-            );
-          } else {
-            // Si no hay valor de búsqueda de número de tramite, aplicar otros filtros
-            return (
-              fila.TipoTramiteServicio.nombre === filtro.nombre ||
-              fila.EstadoTasas.descripcion === filtro.descripcion ||
-              fila.EstadoExcedentes.descripcion === filtro.descripcion ||
-              fila.Jurisdiccion.nombre === filtro.nombre ||
-              fila.EstadoTramite.descripcion === filtro.descripcion
-            );
+     return this.datosTabla.pipe(
+       map(datosTabla => {
+        return datosTabla.filter((fila: any) => {
+          if(filtrosRow.length === 0){
+            return datosTabla
+          }else{
+          return filtrosRow.some((filtro) => {
+              if (filtro.busqueda && filtro.busqueda.descripcion) {
+                // Si hay un valor de búsqueda por número de tramite en los filtros, usarlo para buscar por los últimos dígitos
+                return (
+                  fila.codigo_tramite.endsWith(filtro.busqueda.descripcion) ||
+                  fila.TipoTramiteServicio.nombre === filtro.nombre ||
+                  fila.EstadoTasas.descripcion === filtro.descripcion ||
+                  fila.EstadoExcedentes.descripcion === filtro.descripcion ||
+                  fila.Jurisdiccion.nombre === filtro.nombre ||
+                  fila.EstadoTramite.descripcion === filtro.descripcion
+                );
+              } else {
+                // Si no hay valor de búsqueda de número de tramite, aplicar otros filtros
+                  return (
+                    fila.TipoTramiteServicio.nombre === filtro.nombre ||
+                    fila.EstadoTasas.descripcion === filtro.descripcion ||
+                    fila.EstadoExcedentes.descripcion === filtro.descripcion ||
+                    fila.Jurisdiccion.nombre === filtro.nombre ||
+                    fila.EstadoTramite.descripcion === filtro.descripcion
+                  );
+              }
+          });
           }
-        } else {
-          this.datosTabla.next(this.tablaSinFiltro)
-        }
-      });
-    });
+
+        });
+       })
+     )
+
+
+    //this.datosTabla se convirtió en observable la cual va a ser escuchada por los otros métodos
+    
   }
 
   //convierto las tres tablas en observable de modo que escuchen cada cambio de los tramites
   //y se filtren los datos en las respectivas tablas
 
-  getFinalizado(): Observable<any[]>{
-    return this.tabla$.pipe(
-      map(() => {
-        const tablaFiltrada = this.getTablaFiltrada()
-        return tablaFiltrada.filter((finalizado) =>{
-          return (
-            finalizado.EstadoTramite.descripcion === 'ENTREGADO'
-          )
-        })
-      }
-    ))
+  getFinalizado(): Observable<any[]> {
+    return this.getTablaFiltrada().pipe(
+      map(tablaFiltrada => tablaFiltrada.filter(finalizado =>
+        finalizado.EstadoTramite.descripcion === 'ENTREGADO'
+      ))
+    );
   }
-
+  
   getPendiente(): Observable<any[]> {
-    return this.tabla$.pipe(
-      map(() => {
-        const tablaFiltrada = this.getTablaFiltrada()
-        return tablaFiltrada.filter((pendiente) =>{
-          return (
-            pendiente.EstadoTramite.descripcion === 'NO PRESENTADO' ||
-            pendiente.EstadoTramite.descripcion === 'FINALIZADO CON EXCEDENTES' ||
-            pendiente.EstadoTramite.descripcion === 'OBSERVADO'
-          )
-        })
-      }
-    ))
+    return this.getTablaFiltrada().pipe(
+      map(tablaFiltrada => tablaFiltrada.filter(pendiente =>
+        pendiente.EstadoTramite.descripcion === 'NO PRESENTADO' ||
+        pendiente.EstadoTramite.descripcion === 'FINALIZADO CON EXCEDENTES' ||
+        pendiente.EstadoTramite.descripcion === 'OBSERVADO'
+      ))
+    );
   }
-
+  
   getEntregado(): Observable<any[]> {
-    return this.tabla$.pipe(
-      map(() => {
-        const tablaFiltrada = this.getTablaFiltrada()
-        return tablaFiltrada.filter((entregado) =>{
-          return (   entregado.EstadoTramite.descripcion === 'RECIBIDO' ||
-          entregado.EstadoTramite.descripcion === 'PRESENTADO' ||
-          entregado.EstadoTramite.descripcion === 'FINALIZADO'
-          )
-        })
-      }
-    ))
+    return this.getTablaFiltrada().pipe(
+      map(tablaFiltrada => tablaFiltrada.filter(entregado =>
+        entregado.EstadoTramite.descripcion === 'RECIBIDO' ||
+        entregado.EstadoTramite.descripcion === 'PRESENTADO' ||
+        entregado.EstadoTramite.descripcion === 'FINALIZADO'
+      ))
+    );
   }
+  
 }
