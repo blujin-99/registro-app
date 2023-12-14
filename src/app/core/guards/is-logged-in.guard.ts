88cf2c258c55477171270971b86b24a05eb22e72
Matias Lenci <malenci@santafe.gov.ar>
2023-12-14 09:19:09 -0300

    update auth

--- src/app/core/guards/is-logged-in.guard.ts
+++ src/app/core/guards/is-logged-in.guard.ts
@@ -1,13 +1,26 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
+import { AuthStatus } from '../models';
+import { environment } from 'src/environments/environment';
 
 export const isLoggedInGuard: CanActivateFn = (route, state) => {
   const userSrv = inject(UserService);
 
-  if (userSrv.getUserCas()) {
+  if (userSrv.authStatus() === AuthStatus.authenticated) {
     return true;
   }
+
+  const url = state.url;
+  localStorage.setItem('url', url);
+
+  if (userSrv.authStatus() === AuthStatus.checking) {
+    if (localStorage.getItem('url')) {
+      userSrv.login();
+    }
+    return false;
+  }
+  userSrv.login();
 
   return false;
 };
