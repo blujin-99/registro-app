6fc8cdb5ed03d19df681bdcacc1676094495ac0d
Matias Lenci <malenci@santafe.gov.ar>
2023-12-14 11:29:00 -0300

    abstraccion de guarda para separar logica

--- src/app/core/guards/is-logged-in.guard.ts
+++ src/app/core/guards/is-logged-in.guard.ts
@@ -1,14 +1,11 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { AuthStatus } from '../models';
-import { environment } from 'src/environments/environment';
 
 export const isLoggedInGuard: CanActivateFn = (route, state) => {
   const userSrv = inject(UserService);
 
-  const url = state.url;
-  localStorage.setItem('url', url);
   if (userSrv.authStatus() === AuthStatus.authenticated) {
     return true;
   }
--- src/app/core/interceptor/not-found.interceptor.ts
+++ src/app/core/interceptor/not-found.interceptor.ts
@@ -11,11 +11,13 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs';
 import { CatchinErrorService } from '../services/catchin-error.service';
 import { UserService } from '../services/user.service';
+import { AuthStatus } from '../models';
 @Injectable()
 export class NotFoundError implements HttpInterceptor {
-  constructor(private catchErrorServ: CatchinErrorService, private userService :UserService) {
-  
-  }
+  constructor(
+    private catchErrorServ: CatchinErrorService,
+    private userService: UserService
+  ) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -23,8 +25,11 @@ export class NotFoundError implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
-            this.userService.logout()        
+        if (
+          error.status === 401 &&
+          this.userService.authStatus() === AuthStatus.notAuthenticated
+        ) {
+          this.userService.logout();
         }
         this.catchErrorServ.showError(error.status);
         return throwError(
@@ -34,4 +39,3 @@ export class NotFoundError implements HttpInterceptor {
     );
   }
 }
-
