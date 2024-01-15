1fa8135f720179efa2184d1d6e224a58ecb33a22
Gustavo campos <camposgustavoj@hotmail.com>
2024-01-15 07:26:21 -0300

    -

--- src/app/shared/services/tramites.service.ts
+++ src/app/shared/services/tramites.service.ts
@@ -14,14 +14,30 @@ export class TramitesService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Retorna la lista de tipos de tramites de un usuario
+   *
+   * @returns Observable<IFiltros>
+   */
   getFiltros(): Observable<IFiltros> {
     return this.http.get<IFiltros>(this.env.apiBase + this.env.api.tramitesFiltros);
   }
 
+  /**
+   * Retorna los tramites de un usuario
+   *
+   * @returns Observable<ITramite[]>
+   */
   getTramites():Observable<ITramite[]> {
     return this.http.get<ITramite[]>(this.env.apiBase + this.env.api.tramites);
   }
 
+  /**
+   * Retorna que acciones se puede ejecutar de un determintado tramite, para el usuario
+   *
+   * @param id ID de un trámite
+   * @returns Observable<IAction[]>
+   */
   getTramiteActions(id: number) : Observable<IAction[]> {
     return this.http.get<IAction[]>(this.env.apiBase + this.env.api.actions.replace("{codigo}", id.toString()));
   }
